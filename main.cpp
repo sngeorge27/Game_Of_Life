aa68129dab175b932054fd910e0d3325e6721d0a
sngeorge27 <sngeorge27@gmail.com>
2019-04-24 15:38:11 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /*
- * EECE1080C Spring, 2019 Project 2 Final Submission:
+ * 
  * -> Conway's Game of Life
  *
  * Filename:  main.cpp
@@ -7,7 +7,7 @@
  * This program is the basic concept of Conway's Game of Life. The alterations we made to the game are: allowing the user to select the amount
  * of generations printed, the file content that the program reads, and the parameters of determining which cell is dead or alive.
  * ------------------------------------------------------------------------------
- * |PLEASE USE OUR SAMPLE INPUT (settings.txt) OR USE SAME FORMAT. ~ Thank you. |
+ * |                                                                            |
  * |                                                                            |
  * | settings.txt format:                                                       |
  * |  - Each row is a coordinate (x,y) for an alive of the cell                 |
@@ -16,29 +16,6 @@
  *  Finished:  04/25/2019
  *  Revision:  none
  *  Compiler:  MinGW
- *
- *  Developers:
- *      - Keerthi Sekar - sekarku@mail.uc.edu
- *      - Sarah George - georges6@mail.uc.edu
- *
- * Developer comments:
- * Developer 1 (Keerthi Sekar):
- * Tasks:
- * Main Menu set up (Menu and case structure), modes (determineisAlive1 and 2), Grid Class skeleton, and allowing user to quit whenever
- * they feel like.
- *
- * Learned from experience:
- * How to combine another person's code and ideas into mine. It was interesting to tackle the challenges of combining both of our ideas.
- * I also got a better understanding/practice of when to use classes to solve certain problems.
- *
- * Developer 2 (Sarah George):
- * Tasks:
- * Coord class, member functions determineisAlive, copy, Update, getLive, breakupstring, and updating board in main.
- *
- * Learned from experience:
- * How to effectively collaborate with another individual on a task and combine ideas. I have gained a better understanding of how to
- * implement object-oriented programming using classes. It was interesting to tackle the challenge of using File I/O, combining both developer's
- * ideas, and adding our own creativity to the game.
  *
  */
 
